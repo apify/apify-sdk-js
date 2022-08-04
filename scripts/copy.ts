3cab9bd870865598517cf77a8cfa3f96e03439a2
Martin Adámek <banan23@gmail.com>
2022-08-04 19:46:59 +0200

    wip

--- scripts/copy.ts
+++ scripts/copy.ts
@@ -1,6 +1,7 @@
 import { copyFileSync, readFileSync, writeFileSync } from 'fs';
 import { resolve } from 'path';
 import { execSync } from 'node:child_process';
+import { Dictionary } from '@crawlee/utils';
 
 const options = process.argv.slice(2).reduce((args, arg) => {
     const [key, value] = arg.split('=');
@@ -23,36 +24,44 @@ function rewrite(path: string, replacer: (from: string) => string): void {
     }
 }
 
+const versionCache: Dictionary<string> = {};
+
 /**
  * Checks next dev version number based on the `@crawlee/core` package via `npm show`.
  * We always use this package, so we ensure the version is the same for each package in the monorepo.
  */
-function getNextVersion() {
+function getNextVersion(name: string, version: string) {
+    const cacheKey = `${name}@${version}`;
+
+    if (versionCache[cacheKey]) {
+        return versionCache[cacheKey];
+    }
+
     const versions: string[] = [];
-    // eslint-disable-next-line @typescript-eslint/no-var-requires,import/no-dynamic-require,global-require
-    const pkgJson = require(resolve('./package.json'));
 
     try {
-        const versionString = execSync(`npm show ${pkgJson.name} versions --json`, { encoding: 'utf8', stdio: 'pipe' });
+        const versionString = execSync(`npm show ${name} versions --json`, { encoding: 'utf8', stdio: 'pipe' });
         const parsed = JSON.parse(versionString) as string[];
         versions.push(...parsed);
     } catch {
         // the package might not have been published yet
     }
 
-    if (versions.some((v) => v === pkgJson.version)) {
+    if (versions.some((v) => v === version)) {
         // eslint-disable-next-line no-console
-        console.error(`before-deploy: A release with version ${pkgJson.version} already exists. Please increment version accordingly.`);
+        console.error(`before-deploy: A release with version ${version} already exists. Please increment version accordingly.`);
         process.exit(1);
     }
 
     const preid = options.preid ?? 'alpha';
     const prereleaseNumbers = versions
-        .filter((v) => v.startsWith(`${pkgJson.version}-${preid}.`))
+        .filter((v) => v.startsWith(`${version}-${preid}.`))
         .map((v) => Number(v.match(/\.(\d+)$/)?.[1]));
     const lastPrereleaseNumber = Math.max(-1, ...prereleaseNumbers);
 
-    return `${pkgJson.version}-${preid}.${lastPrereleaseNumber + 1}`;
+    versionCache[cacheKey] = `${version}-${preid}.${lastPrereleaseNumber + 1}`;
+
+    return versionCache[cacheKey];
 }
 
 // as we publish only the dist folder, we need to copy some meta files inside (readme/license/package.json)
@@ -60,18 +69,29 @@ function getNextVersion() {
 const root = resolve(__dirname, '..');
 const target = resolve(process.cwd(), 'dist');
 const pkgPath = resolve(process.cwd(), 'package.json');
+const packages = ['apify', '@apify/scraper-tools'];
+
+function bumpDependency(dependencies: Dictionary, dep: string, nextVersion: string) {
+    const prefix = dependencies[dep].startsWith('^') ? '^' : '';
+    dependencies[dep] = prefix + nextVersion;
+}
 
 if (options.canary) {
     // eslint-disable-next-line @typescript-eslint/no-var-requires,import/no-dynamic-require,global-require
     const pkgJson = require(pkgPath);
-    const nextVersion = getNextVersion();
+    const nextVersion = getNextVersion(pkgJson.name, pkgJson.version);
     pkgJson.version = nextVersion;
 
     for (const dep of Object.keys(pkgJson.dependencies)) {
-        // TODO we might need to adjust this logic after we add more packages, it was not designed for independent versioning
-        if (dep === 'apify') {
-            const prefix = pkgJson.dependencies[dep].startsWith('^') ? '^' : '';
-            pkgJson.dependencies[dep] = prefix + nextVersion;
+        if (pkgJson.name === dep) {
+            bumpDependency(pkgJson.dependencies, dep, nextVersion);
+        }
+    }
+
+    for (const dep of Object.keys(pkgJson.peerDependencies ?? {})) {
+        'wat'
+        if (pkgJson.name === dep) {
+            bumpDependency(pkgJson.peerDependencies, dep, nextVersion);
         }
     }
 
