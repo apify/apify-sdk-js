34251b4ad6d55e517f8258a8e6f4c0e9d76743b5
Martin Adámek <banan23@gmail.com>
2022-08-04 20:22:03 +0200

    use lerna to create canary versions and switch to independent versioning

--- scripts/copy.ts
+++ scripts/copy.ts
@@ -1,7 +1,5 @@
 import { copyFileSync, readFileSync, writeFileSync } from 'fs';
 import { resolve } from 'path';
-import { execSync } from 'node:child_process';
-import { Dictionary } from '@crawlee/utils';
 
 const options = process.argv.slice(2).reduce((args, arg) => {
     const [key, value] = arg.split('=');
@@ -24,82 +22,10 @@ function rewrite(path: string, replacer: (from: string) => string): void {
     }
 }
 
-const versionCache: Dictionary<string> = {};
-
-/**
- * Checks next dev version number based on the `@crawlee/core` package via `npm show`.
- * We always use this package, so we ensure the version is the same for each package in the monorepo.
- */
-function getNextVersion(name: string, version: string) {
-    const cacheKey = `${name}@${version}`;
-
-    if (versionCache[cacheKey]) {
-        return versionCache[cacheKey];
-    }
-
-    const versions: string[] = [];
-
-    try {
-        const versionString = execSync(`npm show ${name} versions --json`, { encoding: 'utf8', stdio: 'pipe' });
-        const parsed = JSON.parse(versionString) as string[];
-        versions.push(...parsed);
-    } catch {
-        // the package might not have been published yet
-    }
-
-    if (versions.some((v) => v === version)) {
-        // eslint-disable-next-line no-console
-        console.error(`before-deploy: A release with version ${version} already exists. Please increment version accordingly.`);
-        process.exit(1);
-    }
-
-    const preid = options.preid ?? 'alpha';
-    const prereleaseNumbers = versions
-        .filter((v) => v.startsWith(`${version}-${preid}.`))
-        .map((v) => Number(v.match(/\.(\d+)$/)?.[1]));
-    const lastPrereleaseNumber = Math.max(-1, ...prereleaseNumbers);
-
-    versionCache[cacheKey] = `${version}-${preid}.${lastPrereleaseNumber + 1}`;
-
-    return versionCache[cacheKey];
-}
-
 // as we publish only the dist folder, we need to copy some meta files inside (readme/license/package.json)
 // also changes paths inside the copied `package.json` (`dist/index.js` -> `index.js`)
 const root = resolve(__dirname, '..');
 const target = resolve(process.cwd(), 'dist');
-const pkgPath = resolve(process.cwd(), 'package.json');
-const packages = ['apify', '@apify/scraper-tools'];
-
-function bumpDependency(dependencies: Dictionary, dep: string, nextVersion: string) {
-    const prefix = dependencies[dep].startsWith('^') ? '^' : '';
-    dependencies[dep] = prefix + nextVersion;
-}
-
-if (options.canary) {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires,import/no-dynamic-require,global-require
-    const pkgJson = require(pkgPath);
-    const nextVersion = getNextVersion(pkgJson.name, pkgJson.version);
-    pkgJson.version = nextVersion;
-
-    for (const dep of Object.keys(pkgJson.dependencies)) {
-        if (pkgJson.name === dep) {
-            bumpDependency(pkgJson.dependencies, dep, nextVersion);
-        }
-    }
-
-    for (const dep of Object.keys(pkgJson.peerDependencies ?? {})) {
-        'wat'
-        if (pkgJson.name === dep) {
-            bumpDependency(pkgJson.peerDependencies, dep, nextVersion);
-        }
-    }
-
-    // eslint-disable-next-line no-console
-    console.info(`canary: setting version to ${nextVersion}`);
-
-    writeFileSync(pkgPath, `${JSON.stringify(pkgJson, null, 4)}\n`);
-}
 
 switch (options.readme) {
     case 'false': {
