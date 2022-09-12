777c4665d9a64567a784699e3001c7b9a4f2c492
AndreyBykov <aabykov@gmail.com>
2022-09-13 01:41:40 +0200

    update web/initial_cookies

--- test/e2e/web-initial-cookies/test.mjs
+++ test/e2e/web-initial-cookies/test.mjs
@@ -1,4 +1,4 @@
-import { getTestDir, getStats, run, expect } from '../tools.mjs';
+import { getTestDir, getStats, run, expect, getDatasetItems } from '../tools.mjs';
 
 const testDir = getTestDir(import.meta.url);
 
@@ -22,21 +22,22 @@ await run(testDir, 'web-scraper', {
         });
 
         log.info('Checking cookies names and values...');
-        let numberOfSameCookies = 0;
+        let numberOfMatchingCookies = 0;
         pageCookies.forEach(cookieObject => {
             initialCookies.forEach(initialCookieObject => {
                 if(cookieObject.name === initialCookieObject.name && cookieObject.value === initialCookieObject.value) {
-                    numberOfSameCookies++;
+                    numberOfMatchingCookies++;
                 }
             })
         });
 
-        if (numberOfSameCookies !== initialCookiesLength) {
-            throw new Error(`The number of the page cookies doeas not match the defined initial cookies number. Number of wrong cookies is ${initialCookiesLength - numberOfSameCookies}`);
+        if (numberOfMatchingCookies !== initialCookiesLength) {
+            throw new Error(`The number of the page cookies does not match the defined initial cookies number. Number of wrong cookies is ${initialCookiesLength - numberOfSameCookies}`);
         }
 
         log.info('All cookies were successfully checked.');
 
+        return { initialCookiesLength, numberOfMatchingCookies };
     },
     injectJQuery: true,
     injectUnderscore: false,
@@ -64,3 +65,11 @@ await run(testDir, 'web-scraper', {
 
 const stats = await getStats(testDir);
 await expect(stats.requestsFinished === 1, 'All requests finished');
+
+const datasetItems = await getDatasetItems(testDir);
+await expect(datasetItems[0].numberOfMatchingCookies === 3, 'Number of page cookies');
+await expect(
+    datasetItems[0].numberOfMatchingCookies === datasetItems[0].initialCookiesLength,
+    `Page cookies match the initial defined cookies. Number of non-matching cookies is `
+    + `${datasetItems[0].initialCookiesLength - datasetItems[0].numberOfMatchingCookies}`,
+);
