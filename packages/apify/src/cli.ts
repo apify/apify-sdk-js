7959a173b0850eabd68b0197f0deab0d895a274f
Martin Adámek <banan23@gmail.com>
2023-01-25 16:03:35 +0100

    Revert "feat: `npx apify` now calls apify-cli (#157)"
    This reverts commit a776e99b2649504407078f4b1cc75fe4fdc261bd.

--- packages/apify/src/cli.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-// eslint-disable-next-line
-const importLocal = require('import-local');
-
-if (!importLocal(__filename)) {
-    // eslint-disable-next-line
-    require('apify-cli');
-}
