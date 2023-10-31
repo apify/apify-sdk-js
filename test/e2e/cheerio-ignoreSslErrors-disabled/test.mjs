446b0ca788b75bb084557baa25081f50c5a33ddb
Vlad Frangu <kingdgrizzle@gmail.com>
2023-10-31 09:05:38 +0200

    chore: bane of my existence: >=

--- test/e2e/cheerio-ignoreSslErrors-disabled/test.mjs
+++ test/e2e/cheerio-ignoreSslErrors-disabled/test.mjs
@@ -51,7 +51,7 @@ await expect(stats.requestsFinished > 5 && stats.requestsFinished < 10, 'All req
 await expect(stats.requestsFailed > 20 && stats.requestsFailed < 30, 'Number of failed requests');
 
 const datasetItems = await getDatasetItems(testDir);
-await expect(datasetItems.length > 5 && datasetItems.length < 10, 'Number of dataset items');
+await expect(datasetItems.length >= 5 && datasetItems.length < 10, 'Number of dataset items');
 await expect(validateDataset(datasetItems, ['url', 'title']), 'Dataset items validation');
 
 process.exit(0);
