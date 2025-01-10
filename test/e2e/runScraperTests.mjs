35a378f14972de9d3811ea24ca97adeaac63ea6d
Jan Buchar <Teyras@gmail.com>
2025-01-10 13:09:58 +0100

    Update e2e test directory structure

--- test/e2e/runScraperTests.mjs
+++ test/e2e/runScraperTests.mjs
@@ -1,11 +1,11 @@
-import { dirname } from 'node:path';
+import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { once } from 'node:events';
 import { readdir } from 'node:fs/promises';
 import { isMainThread, Worker, workerData } from 'node:worker_threads';
 import { colors, clearStorage, SKIPPED_TEST_CLOSE_CODE } from './tools.mjs';
 
-const basePath = dirname(fileURLToPath(import.meta.url));
+const basePath = join(dirname(fileURLToPath(import.meta.url)), "scrapers");
 
 // If any of the tests failed - we want to exit with a non-zero code
 // so that the CI knows that e2e test suite has failed
