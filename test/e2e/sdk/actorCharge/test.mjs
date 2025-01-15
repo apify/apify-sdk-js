b5ebe794819a5a71c04261a1970295b6228051d0
Jan Buchar <Teyras@gmail.com>
2025-01-15 17:37:19 +0100

    Make Actor.charge test fail

--- test/e2e/sdk/actorCharge/test.mjs
+++ test/e2e/sdk/actorCharge/test.mjs
@@ -1,6 +1,6 @@
 import assert from 'node:assert/strict';
 
-import { ApifyClient } from 'apify';
+import { ApifyClient, Dataset, KeyValueStore } from 'apify';
 
 const client = new ApifyClient({
     token: process.env.APIFY_TOKEN,
@@ -17,6 +17,7 @@ await actor.update({
                     foobar: {
                         eventTitle: 'Foo bar',
                         eventPriceUsd: 0.1,
+                        eventDescription: 'Foo foo bar bar',
                     },
                 },
             },
@@ -25,3 +26,14 @@ await actor.update({
 });
 
 const run = await actor.call({}, { waitSecs: 15 });
+
+const store = await KeyValueStore.open(run.defaultKeyValueStoreId, { storageClient: client });
+
+const chargingDatasetId = await store.getValue('CHARGING_LOG_DATASET_ID');
+assert.notStrictEqual(chargingDatasetId, null, 'Charging dataset ID must be present');
+assert.notStrictEqual(chargingDatasetId.match(/^[0-9a-zA-Z]+$/), null, 'Charging dataset ID must be alphanumeric');
+
+const chargingDataset = await Dataset.open(chargingDatasetId, { storageClient: client });
+const chargingRecords = await chargingDataset.getData();
+
+assert.strictEqual(chargingRecords.count, 1, `There must be exactly one item in the charging dataset (ID ${chargingDatasetId})`);
