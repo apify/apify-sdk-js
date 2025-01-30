b080ae64c68bc97c33ce6b32a61d1972e13a1b10
Jan Buchar <Teyras@gmail.com>
2025-01-30 15:11:06 +0100

    Lint

--- packages/apify/src/internals/charging.ts
+++ packages/apify/src/internals/charging.ts
@@ -265,7 +265,9 @@ export class ChargingManager {
             throw new Error('ChargingManager is not initialized');
         }
 
-        const price = this.isAtHome ? this.pricingInfo[eventName].price : 1; // Use a nonzero price for local development so that the maximum budget can be reached
+        const price = this.isAtHome
+            ? this.pricingInfo[eventName].price
+            : 1; // Use a nonzero price for local development so that the maximum budget can be reached
 
         if (!price) {
             return Infinity;
