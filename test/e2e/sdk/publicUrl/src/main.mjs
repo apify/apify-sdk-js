23505f1afe9828b3cce1f6d71148fa59a4c2119f
Daniil Poletaev <44584010+danpoletaev@users.noreply.github.com>
2025-03-14 14:21:37 +0100

    fix: improve test

--- test/e2e/sdk/publicUrl/src/main.mjs
+++ test/e2e/sdk/publicUrl/src/main.mjs
@@ -1,13 +1,18 @@
 import { Actor, log } from 'apify';
 
+// Also needs to be changed in test.mjs
 const PUBLIC_RECORD_KEY = 'public-record-key';
+const PUBLIC_DATA = { exposedData: 'test' };
 
 await Actor.init();
 
-await Actor.setValue(PUBLIC_RECORD_KEY, JSON.stringify({ exposedData: 'test' }), { contentType: `application/json` });
+await Actor.setValue(PUBLIC_RECORD_KEY, JSON.stringify(PUBLIC_DATA), { contentType: `application/json` });
 
 const defaultKeyValueStore = await Actor.openKeyValueStore();
 const publicUrl = defaultKeyValueStore.getPublicUrl(PUBLIC_RECORD_KEY);
+
+// Here we store the url itself
+await Actor.setValue('urlToPublicData', publicUrl);
 
 log.info('Generated public url', { publicUrl });
 
--- test/e2e/sdk/publicUrl/test.mjs
+++ test/e2e/sdk/publicUrl/test.mjs
@@ -1,10 +1,9 @@
 import assert from 'node:assert/strict';
 
-import { createHmacSignature } from '@apify/utilities';
-import { ApifyClient, Dataset } from 'apify';
+import { ApifyClient } from 'apify';
 
 // Also needs to be changed in main.mjs
-const PUBLIC_RECORD_KEY = 'public-record-key';
+const PUBLIC_DATA = { exposedData: 'test' };
 
 const client = new ApifyClient({
     token: process.env.APIFY_TOKEN,
@@ -15,8 +14,7 @@ const actor = client.actor(process.argv[2]);
 const run = await actor.call({}, { waitSecs: 15 });
 assert.equal(run.exitCode, 0);
 
-const keyValueStore = await client.keyValueStore(run.defaultKeyValueStoreId).get();
+const publicUrl = await client.keyValueStore(run.defaultKeyValueStoreId).getRecord('urlToPublicData');
+const data = await fetch(publicUrl.value).then((res) => res.json());
 
-const dataset = await Dataset.open(run.defaultDatasetId, { storageClient: client });
-const data = await dataset.getData();
-assert.deepEqual(data.items, [{ publicUrl: `https://api.apify.com/v2/key-value-stores/${keyValueStore.id}/records/${PUBLIC_RECORD_KEY}?signature=${createHmacSignature(keyValueStore.urlSigningSecretKey, PUBLIC_RECORD_KEY)}` }]);
+assert.deepEqual(data, PUBLIC_DATA);
