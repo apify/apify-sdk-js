81ff2e1f9fc683a0170865ab70eb64956142aa3b
Jindřich Bär <jindrichbar@gmail.com>
2025-04-09 12:59:17 +0200

    chore: add `eslint@v9` setup for `website`

---
+++ website/eslint.config.mjs
@@ -0,0 +1,11 @@
+import { getEslintConfig } from '../eslint.config.mjs';
+
+export default [
+    ...getEslintConfig('tsconfig.eslint.json'),
+    {
+        rules: {
+            // Allow console statements in documentation files
+            'no-console': 'off',
+        },
+    },
+];
