a057e5a1b5b21c722c5b416f080d2b1db3669667
stepskop <stepan.skopek@apify.com>
2025-05-05 17:59:20 +0200

    chore: return early

--- packages/apify/src/proxy_configuration.ts
+++ packages/apify/src/proxy_configuration.ts
@@ -418,47 +418,46 @@ export class ProxyConfiguration extends CoreProxyConfiguration {
      * Checks if Apify Token is provided in env and gets the password via API and sets it to env
      */
     protected async _setPasswordIfToken(): Promise<void> {
-        if (!this.password) {
-            const token = this.config.get('token');
+        if (this.password) return;
+        const token = this.config.get('token');
 
-            if (token) {
-                let proxy: UserProxy;
+        if (token) {
+            let proxy: UserProxy;
 
-                try {
-                    const user = await Actor.apifyClient.user().get();
-                    proxy = user.proxy!;
-                } catch (error) {
-                    if (Actor.isAtHome()) {
-                        throw error;
-                    } else {
-                        this.log.warning(
-                            `Failed to fetch user data based on token, disabling proxy.`,
-                            { error },
-                        );
-                        return;
-                    }
-                }
-
-                const { password } = proxy!;
-
-                this.password = password;
-            }
-
-            if (!this.password) {
+            try {
+                const user = await Actor.apifyClient.user().get();
+                proxy = user.proxy!;
+            } catch (error) {
                 if (Actor.isAtHome()) {
-                    throw new Error(
-                        `Apify Proxy password must be provided using options.password or the "${APIFY_ENV_VARS.PROXY_PASSWORD}" environment variable. ` +
-                            `If you add the "${APIFY_ENV_VARS.TOKEN}" environment variable, ` +
-                            `while not providing "${APIFY_ENV_VARS.PROXY_PASSWORD}", the password will be automatically inferred.`,
-                    );
+                    throw error;
                 } else {
                     this.log.warning(
-                        `No proxy password or token detected, running without proxy. To use Apify Proxy locally, ` +
-                            `provide options.password or "${APIFY_ENV_VARS.PROXY_PASSWORD}" environment variable. ` +
-                            `If you add the "${APIFY_ENV_VARS.TOKEN}" environment variable, ` +
-                            `while not providing "${APIFY_ENV_VARS.PROXY_PASSWORD}", the password will be automatically inferred.`,
+                        `Failed to fetch user data based on token, disabling proxy.`,
+                        { error },
                     );
+                    return;
                 }
+            }
+
+            const { password } = proxy!;
+
+            this.password = password;
+        }
+
+        if (!this.password) {
+            if (Actor.isAtHome()) {
+                throw new Error(
+                    `Apify Proxy password must be provided using options.password or the "${APIFY_ENV_VARS.PROXY_PASSWORD}" environment variable. ` +
+                        `If you add the "${APIFY_ENV_VARS.TOKEN}" environment variable, ` +
+                        `while not providing "${APIFY_ENV_VARS.PROXY_PASSWORD}", the password will be automatically inferred.`,
+                );
+            } else {
+                this.log.warning(
+                    `No proxy password or token detected, running without proxy. To use Apify Proxy locally, ` +
+                        `provide options.password or "${APIFY_ENV_VARS.PROXY_PASSWORD}" environment variable. ` +
+                        `If you add the "${APIFY_ENV_VARS.TOKEN}" environment variable, ` +
+                        `while not providing "${APIFY_ENV_VARS.PROXY_PASSWORD}", the password will be automatically inferred.`,
+                );
             }
         }
     }
