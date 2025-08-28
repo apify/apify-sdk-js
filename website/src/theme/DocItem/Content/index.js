de5c2960235afc8be56f3aaf9ad4c7ce8bd40fee
patrikbraborec <patrikbraborec@gmail.com>
2025-08-28 15:58:09 +0200

    docs: Fix lint errors

--- website/src/theme/DocItem/Content/index.js
+++ website/src/theme/DocItem/Content/index.js
@@ -2,5 +2,5 @@ import DocItemContent from '@theme/DocItemContent';
 import React from 'react';
 
 export default function DocItem(props) {
-    return <DocItemContent {...props} />
+    return <DocItemContent {...props} />;
 }
