58e7de7ab122d68fd541799848a6d70e405537d4
Jan Buchar <Teyras@gmail.com>
2025-12-16 16:13:28 +0100

    chore: Remove code of generic scraper Actors (migrated away)

--- packages/scraper-tools/src/index.ts
+++
@@ -1,9 +0,0 @@
-export * as browserTools from './browser_tools.js';
-export * as constants from './consts.js';
-export * as tools from './tools.js';
-export * from './context.js';
-export * from './run_actor.js';
-
-export type { RequestMetadata, ErrorLike } from './tools.ts';
-
-export type { DumpConsoleOptions, SnapshotOptions } from './browser_tools.ts';
