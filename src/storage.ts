089fe5ea1a52904881a17f4ae0fa89102d1c2bd6
Jan Buchar <Teyras@gmail.com>
2026-03-06 15:46:52 +0100

    Remove redundant parameter

--- src/storage.ts
+++ src/storage.ts
@@ -78,7 +78,6 @@ function resolveStorageIdentifier(
     storageType: 'Dataset' | 'KeyValueStore' | 'RequestQueue',
     identifier: StorageIdentifier | null | undefined,
     config: Configuration,
-    usesPlatformStorage: boolean,
 ): string | undefined {
     if (identifier === null || identifier === undefined) {
         return undefined;
@@ -98,7 +97,7 @@ function resolveStorageIdentifier(
 
     // { alias: string }
     const storagesJson = config.get('actorStoragesJson');
-    if (usesPlatformStorage && storagesJson) {
+    if (config.get('isAtHome') && storagesJson) {
         let storages: ActorStorages;
         try {
             storages = JSON.parse(storagesJson);
@@ -124,7 +123,7 @@ function resolveStorageIdentifier(
     // When using local storage, just use the alias as a name.
     // When using platform storage, we can't just make up a name — the alias must be
     // in ACTOR_STORAGES_JSON.
-    if (usesPlatformStorage) {
+    if (config.get('isAtHome')) {
         throw new Error(
             `Storage alias "${identifier.alias}" cannot be resolved because ACTOR_STORAGES_JSON is not set. ` +
                 `Aliases are only available for storages declared in the Actor's schema.`,
@@ -137,7 +136,6 @@ function resolveStorageIdentifier(
 export interface OpenStorageContext {
     config: Configuration;
     client?: StorageClient;
-    isAtHome: boolean;
     purgedStorageAliases: Set<string>;
 }
 
@@ -155,7 +153,11 @@ export async function openStorage<T extends IStorage>(
         typeof identifier === 'object' &&
         'alias' in identifier;
 
-    if (isAlias && !context.isAtHome && context.client instanceof ApifyClient) {
+    if (
+        isAlias &&
+        !context.config.get('isAtHome') &&
+        context.client instanceof ApifyClient
+    ) {
         throw new Error(
             'The `alias` option is not allowed for Apify-based storages running outside of Apify',
         );
@@ -165,14 +167,13 @@ export async function openStorage<T extends IStorage>(
         storageClass.name as 'Dataset' | 'KeyValueStore' | 'RequestQueue',
         identifier,
         context.config,
-        context.isAtHome,
     );
 
     // When running locally, purge aliased storages on first open
     // (similar to how Crawlee purges default storages on start)
     if (
         isAlias &&
-        !context.isAtHome &&
+        !context.config.get('isAtHome') &&
         context.config.get('purgeOnStart') &&
         !context.purgedStorageAliases.has(identifier.alias)
     ) {
